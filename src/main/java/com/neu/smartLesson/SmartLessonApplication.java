020ad64e40e42d939c690228ba5a2ca982857f86
ZBoyn <279782706@qq.com>
2025-11-10 20:32:40 +0800

    Init smartLessonBackend

---
+++ src/main/java/com/neu/smartLesson/SmartLessonApplication.java
@@ -0,0 +1,13 @@
+package com.neu.smartLesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLessonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartLessonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neu/smartLesson/SmartLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neu.smartLesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
